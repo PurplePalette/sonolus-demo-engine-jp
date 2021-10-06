cd69d0a848f5c397b752d57d6a78398361d4ef0d
NonSpicyBurrito <burritoyangster@gmail.com>
2021-10-06 09:05:04 +0800

    Ch.07

---
+++ src/engine/data/archetypes.ts
@@ -0,0 +1,8 @@
+import { defineArchetypes } from 'sonolus.js'
+import { scripts } from './scripts'
+
+export const archetypes = defineArchetypes({
+    initialization: {
+        script: scripts.initializationIndex,
+    },
+})
