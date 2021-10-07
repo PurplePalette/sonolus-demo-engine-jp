0c9b67e09a5ac4aa5c14865c8f9900fc9f97bed1
NonSpicyBurrito <burritoyangster@gmail.com>
2021-10-07 09:07:48 +0800

    Ch.23

--- src/engine/data/scripts/note.ts
+++ src/engine/data/scripts/note.ts
@@ -18,12 +18,14 @@ import {
     Multiply,
     Not,
     Or,
+    ParticleEffect,
     Play,
     Pointer,
     Random,
     Remap,
     Script,
     SkinSprite,
+    SpawnParticleEffect,
     Subtract,
     TemporaryMemory,
     Time,
@@ -90,6 +92,19 @@ export function note(): Script {
             InputBucketValue.set(Multiply(1000, InputAccuracy)),
 
             Play(Add(EffectClip.Miss, InputJudgment), 0.02),
+            SpawnParticleEffect(
+                ParticleEffect.NoteCircularTapCyan,
+                -0.4,
+                -1,
+                -0.4,
+                -0.2,
+                0.4,
+                -0.2,
+                0.4,
+                -1,
+                0.3,
+                false
+            ),
         ]
     )
 
