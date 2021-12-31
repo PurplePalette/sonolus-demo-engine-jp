71d0943a2d505ee9011730adce56d3711a2b775c
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-12-31 23:55:05 +0900

    📝 Add comments to initialization

--- src/engine/data/scripts/initialization.ts
+++ src/engine/data/scripts/initialization.ts
@@ -28,22 +28,48 @@ import {
 import { archetypes } from '../archetypes'
 import { buckets } from '../buckets'
 
+/**
+ * エンジンの初期化スクリプト
+*/
 export function initialization(): Script {
+    // 定義済みのノーツエンティティ型のポインタを渡して
+    // EntityInfoArrayからアーキタイプライフ型のポインタを取得する
+    //   ※ ArchetypeLifeは自動的にEntityInfoArrayを参照する
+    //   ※ これにより指定したノーツの判定発生時に行うライフ処理を定義できる
+    // https://wiki.sonolus.com/sonolus.js-guide/ja/chapters/05.html
+
+    // pointer = (load entityArray internal) and return as archetypeLife
+    //   from specified ArcheTypePointer
     const noteLife = ArchetypeLife.of(archetypes.noteIndex)
 
+    /**
+     * 事前処理
+    */
     const preprocess = [
+        // メニューボタンの配置
         UIMenu.set(
+            // anchorX (X position ?)
             Subtract(0.05, ScreenAspectRatio),
+            // anchorY (Y position ?)
             0.95,
+            // pivotX ( animation center pos?)
             0,
+            // pivotY ( animation center pos?)
             1,
+            // width 横幅
             Multiply(UIMenuConfiguration.scale, 0.15),
+            // height 縦幅
             Multiply(UIMenuConfiguration.scale, 0.15),
+            // rotation 回転角度
             0,
+            // alpha 不透明度
             UIMenuConfiguration.alpha,
+            // horizontalAlign 透明度
             HorizontalAlign.Center,
+            // background 背景があるかどうか
             true
         ),
+        // 判定表示の配置
         UIJudgment.set(
             0,
             -0.4,
@@ -56,6 +82,7 @@ export function initialization(): Script {
             HorizontalAlign.Center,
             false
         ),
+        // コンボ値の配置
         UIComboValue.set(
             Multiply(ScreenAspectRatio, 0.7),
             0,
@@ -68,6 +95,7 @@ export function initialization(): Script {
             HorizontalAlign.Center,
             false
         ),
+        // コンボ値上テキストの配置
         UIComboText.set(
             Multiply(ScreenAspectRatio, 0.7),
             0,
@@ -80,6 +108,7 @@ export function initialization(): Script {
             HorizontalAlign.Center,
             false
         ),
+        // プライマリメトリクスの配置
         UIPrimaryMetricBar.set(
             Subtract(ScreenAspectRatio, 0.05),
             0.95,
@@ -92,6 +121,7 @@ export function initialization(): Script {
             HorizontalAlign.Left,
             true
         ),
+        // プライマリメトリクス内容の配置
         UIPrimaryMetricValue.set(
             Subtract(ScreenAspectRatio, 0.05),
             0.95,
@@ -104,6 +134,7 @@ export function initialization(): Script {
             HorizontalAlign.Right,
             false
         ),
+        // セカンダリメトリクスの配置
         UISecondaryMetricBar.set(
             Subtract(ScreenAspectRatio, 0.05),
             Subtract(0.9, Multiply(UIPrimaryMetricConfiguration.scale, 0.15)),
@@ -116,6 +147,7 @@ export function initialization(): Script {
             HorizontalAlign.Left,
             true
         ),
+        // セカンダリメトリクス内容の配置
         UISecondaryMetricValue.set(
             Subtract(ScreenAspectRatio, 0.05),
             Subtract(0.9, Multiply(UIPrimaryMetricConfiguration.scale, 0.15)),
@@ -129,30 +161,50 @@ export function initialization(): Script {
             false
         ),
 
+        // 定義済みのバケットのIndex番号を取って
+        // EntityInfoArrayから指定したノーツエンティティの
+        // レベルバケットのポインタを取得して
+        // バケットの範囲を指定する
         LevelBucket.of(buckets.noteIndex).setBucket(50, 100, 200),
 
+        // 判定タイプ別 スコア乗数
         PerfectMultiplier.set(1),
         GreatMultiplier.set(0.75),
         GoodMultiplier.set(0.5),
-
+        // 判定タイプ 連続数別 スコア乗数
         ConsecutiveGreatScore.set(0.01, 10, 50),
 
-        noteLife.perfectLifeIncrement.set(10),
-        noteLife.missLifeIncrement.set(-100),
-
+        // パーフェクト発生時ライフを0加算
+        noteLife.perfectLifeIncrement.set(0),
+        // ミス発生時ライフを0加算
+        noteLife.missLifeIncrement.set(0),
+        // パーフェクト連続数別 ライフ加算
         ConsecutivePerfectLife.set(50, 10),
     ]
+
+    /**
+     * スポーン順序
+     * 初期化スクリプトは必ず最初にスポーンするため0番目に設定
+    */
     const spawnOrder = 0
 
+    /**
+     * 並列描画処理
+     * このスクリプトは (並列ではなく)逐次処理で実行する
+     * 事前実行が終わると即時終了する (trueが返ると終了なので)
+    */
     const updateSequential = true
 
     return {
+        // 事前処理
         preprocess: {
             code: preprocess,
         },
+        // スポーン順序
         spawnOrder: {
             code: spawnOrder,
         },
+        // 並列実行コールバック(このスクリプトでは即時終了)
         updateSequential: {
             code: updateSequential,
         },
