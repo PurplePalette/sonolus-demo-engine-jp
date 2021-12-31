b76d13dd4ab1c3a71ef594c56cfa268db34f7225
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-12-31 23:55:23 +0900

    📝 Add comments to note.ts

--- src/engine/data/scripts/note.ts
+++ src/engine/data/scripts/note.ts
@@ -35,15 +35,27 @@ import {
 import { options } from '../../configuration/options'
 import { buckets } from '../buckets'
 
+// ノーツの構造体(LevelDataから代入されるデータに名前を付けている)
 class EntityDataPointer extends Pointer {
     public get time() {
         return this.to<number>(0)
     }
 }
 
+// EntityData: Levelからパラメータを受け取れる唯一のメモリ
+//  独自で構造を定義できるポインタ
+//  主に 描画ループなどで使うことが多い
+//  preprocessとspawnOrder以外では読み取り専用
 const EntityData = createEntityData(EntityDataPointer)
 
+/**
+ * ノーツエンティティのスクリプト
+*/
 export function note(): Script {
+    // EntityMemory: 汎用読み書きメモリ
+    //  単一のノーツエンティティ内で扱う事が多い
+    //  各コールバック間でデータを受け渡すためのポインタ定義
+    //  基本的にどこでも読み書きできる
     const spawnTime = EntityMemory.to<number>(0)
     const z = EntityMemory.to<number>(1)
     const minInputTime = EntityMemory.to<number>(2)
@@ -52,31 +64,51 @@ export function note(): Script {
     const yCurrent = EntityMemory.to<number>(32)
     const inputState = EntityMemory.to<boolean>(33)
 
+    /**
+     * 事前処理
+    */
     const preprocess = [
+        // 画面への出現タイミングを計算し代入
         EntityData.time.set(Divide(EntityData.time, options.speed)),
 
+        // エンティティのスポーンタイミングを計算し代入
         spawnTime.set(
             Subtract(EntityData.time, If(options.random, Random(0.5, 1.5), 1))
         ),
+        // Zファイティング対策
         z.set(Subtract(1000, EntityData.time)),
+        // 最早入力受付時刻
         minInputTime.set(Add(EntityData.time, -0.2, InputOffset)),
+        // 最遅入力受付時刻
         maxInputTime.set(Add(EntityData.time, 0.2, InputOffset)),
     ]
 
+    // スポーン順序 (スポーン時刻 + 1000)
     const spawnOrder = Add(spawnTime, 1000)
 
+    // スポーン条件 (時間が来たらスポーン)
     const shouldSpawn = GreaterOr(Time, spawnTime)
 
+    // TemporaryMemory: タッチコールバック内でのみ使用可能なメモリ
+    //  タッチコールバック内以外では読み書きできない
     const isTouchOccupied = TemporaryMemory.to<boolean>(0)
 
+    /**
+     * タッチコールバック処理
+    */
     const touch = And(
+        // タッチが開始した直後
         TouchStarted,
+        // タッチを開始した時間が 最早入力受付時刻以上か
         GreaterOr(TouchST, minInputTime),
+        // タッチ中でない
         Not(isTouchOccupied),
         [
+            // タッチ中に変更
             inputState.set(true),
             isTouchOccupied.set(true),
 
+            // 自動判定
             InputJudgment.set(
                 Judge(
                     Subtract(TouchST, InputOffset),
@@ -86,14 +118,21 @@ export function note(): Script {
                     0.2
                 )
             ),
+            // 精度データ入力
             InputAccuracy.set(Subtract(TouchST, InputOffset, EntityData.time)),
 
+            // バケットデータ入力
             InputBucket.set(buckets.noteIndex),
             InputBucketValue.set(Multiply(1000, InputAccuracy)),
 
+            // 効果音再生
             Play(Add(EffectClip.Miss, InputJudgment), 0.02),
+
+            // パーティクル表示
             SpawnParticleEffect(
+                // ID
                 ParticleEffect.NoteCircularTapCyan,
+                // 座標...?
                 -0.4,
                 -1,
                 -0.4,
@@ -102,51 +141,72 @@ export function note(): Script {
                 -0.2,
                 0.4,
                 -1,
+                // 表示秒数
                 0.3,
+                // ループするかどうか
                 false
             ),
         ]
     )
 
+    // 定数
     const radius = 0.2
     const yFrom = 1 + radius
     const yTo = -0.6
 
     const left = -radius
     const right = radius
+    // この値は 描画中に更新される
     const top = Add(yCurrent, radius)
     const bottom = Subtract(yCurrent, radius)
 
-    const updateParallel = Or(inputState, Greater(Time, maxInputTime), [
-        yCurrent.set(Remap(spawnTime, EntityData.time, yFrom, yTo, Time)),
-        Draw(
-            SkinSprite.NoteHeadCyan,
-            left,
-            bottom,
-            left,
-            top,
-            right,
-            top,
-            right,
-            bottom,
-            z,
-            1
-        ),
-    ])
+    /**
+     * 並列描画処理
+    */
+    const updateParallel = Or(
+        // 入力中
+        inputState,
+        // 最遅入力受付時間を超えた
+        Greater(Time, maxInputTime),
+        // 条件が満たされてる場合実行
+        [
+            // Y座標に新しい地点を代入
+            yCurrent.set(Remap(spawnTime, EntityData.time, yFrom, yTo, Time)),
+            // スプライトを描画
+            Draw(
+                SkinSprite.NoteHeadCyan,
+                left,
+                bottom,
+                left,
+                top,
+                right,
+                top,
+                right,
+                bottom,
+                z,
+                1
+            ),
+        ]
+    )
 
     return {
+        // 事前処理
         preprocess: {
             code: preprocess,
         },
+        // スポーン順序
         spawnOrder: {
             code: spawnOrder,
         },
+        // スポーン条件
         shouldSpawn: {
             code: shouldSpawn,
         },
+        // タッチコールバック
         touch: {
             code: touch,
         },
+        // 並列描画コールバック
         updateParallel: {
             code: updateParallel,
         },
